41c67cda21a673c640011c2e50da5c14b2b73188
jaspindersingh83 <jaspindersingh83@gmail.com>
2024-09-04 17:08:53 +0530

    Completed Project

--- Users/routes.js
+++
@@ -1,9 +0,0 @@
-const { createUser } = require("./UserController");
-
-module.exports = (server) => {
-  server.get("/", (req, res) => {
-    res.status(200).json({ message: "API running" });
-  });
-
-  server.post("/users", createUser);
-};
